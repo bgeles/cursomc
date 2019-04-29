871b28623431647038ad9002405df2246366d226
Bruno Glauco Eles <bglauco@corpus.com.br>
2019-04-29 09:37:42 -0300

    Adaptações classes domain

--- src/main/java/com/brunoglauco/cursomc/services/CategoriaService.java
+++ src/main/java/com/brunoglauco/cursomc/services/CategoriaService.java
@@ -1,12 +1,20 @@
 package com.brunoglauco.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.brunoglauco.cursomc.domain.Categoria;
+import com.brunoglauco.cursomc.domain.Cliente;
+import com.brunoglauco.cursomc.dto.CategoriaDTO;
 import com.brunoglauco.cursomc.repositories.CategoriaRepository;
+import com.brunoglauco.cursomc.services.exceptions.DataIntegrityException;
 import com.brunoglauco.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +28,44 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos associados");
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return  repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+	
 }
