f096cd3b4167dc91f8ed9aedfca68a71afccd1a4
Bruno Glauco Eles <bglauco@corpus.com.br>
2019-04-29 17:30:06 -0300

    email nao repetido na insercao de cliente

--- src/main/java/com/brunoglauco/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/brunoglauco/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,8 +6,10 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.brunoglauco.cursomc.domain.enums.TipoCliente;
 import com.brunoglauco.cursomc.dto.ClienteNewDTO;
 import com.brunoglauco.cursomc.resources.exceptions.FieldMessage;
+import com.brunoglauco.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	@Override
@@ -22,6 +24,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("tipo", "TIpo não pode ser nulo"));
 		}
 		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && BR.isValidCNPJ(objDto.getCpfOuCnpj())){
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && BR.isValidCNPJ(objDto.getCpfOuCnpj())){
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
