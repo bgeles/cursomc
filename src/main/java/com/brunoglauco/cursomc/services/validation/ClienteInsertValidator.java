55fe7b4cc9f7a6a70148352fba2e47373bf6f4f0
Bruno Glauco Eles <bglauco@corpus.com.br>
2019-04-29 17:06:07 -0300

    Validação customizada de CPF e CNPJ na inserção de Cliente

---
+++ src/main/java/com/brunoglauco/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,34 @@
+package com.brunoglauco.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.brunoglauco.cursomc.dto.ClienteNewDTO;
+import com.brunoglauco.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo() == null) {
+			list.add(new FieldMessage("tipo", "TIpo não pode ser nulo"));
+		}
+		
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+
+		return list.isEmpty();
+	}
+}
