3f41c22345bbdf12b7bdf333bcf4bb070dad46de
Bruno Glauco Eles <bglauco@corpus.com.br>
2019-04-30 16:38:45 -0300

    Validação atualizada e email corrigido na atualiza~ção do cliente)

--- src/main/java/com/brunoglauco/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/brunoglauco/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.brunoglauco.cursomc.domain.Cliente;
 import com.brunoglauco.cursomc.domain.enums.TipoCliente;
 import com.brunoglauco.cursomc.dto.ClienteNewDTO;
+import com.brunoglauco.cursomc.repositories.ClienteRepository;
 import com.brunoglauco.cursomc.resources.exceptions.FieldMessage;
 import com.brunoglauco.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +38,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && BR.isValidCNPJ(objDto.getCpfOuCnpj())){
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
---
+++ src/main/java/com/brunoglauco/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.brunoglauco.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.brunoglauco.cursomc.domain.Cliente;
+import com.brunoglauco.cursomc.dto.ClienteDTO;
+import com.brunoglauco.cursomc.repositories.ClienteRepository;
+import com.brunoglauco.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String,String> map = (Map<String,String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+
+		return list.isEmpty();
+	}
+}
