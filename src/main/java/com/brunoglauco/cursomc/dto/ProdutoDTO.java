f47a2a72a0d3f64af5adf953073eacc9db127b3d
Bruno Glauco Eles <bgeles@gemail.com>
2019-05-23 17:55:26 -0300

    teste

--- src/main/java/com/brunoglauco/cursomc/dto/ProdutoDTO.java
+++ src/main/java/com/brunoglauco/cursomc/dto/ProdutoDTO.java
@@ -1,5 +1,67 @@
 package com.brunoglauco.cursomc.dto;
 
-public class ProdutoDTO {
+import java.io.Serializable;
 
+public class ProdutoDTO  implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String Nome;
+	private Double preço;
+	
+	public ProdutoDTO() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProdutoDTO other = (ProdutoDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/main/java/com/brunoglauco/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/brunoglauco/cursomc/resources/ProdutoResource.java
@@ -1,14 +1,18 @@
 package com.brunoglauco.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.brunoglauco.cursomc.domain.Pedido;
+import com.brunoglauco.cursomc.domain.Categoria;
 import com.brunoglauco.cursomc.domain.Produto;
+import com.brunoglauco.cursomc.dto.CategoriaDTO;
+import com.brunoglauco.cursomc.dto.ProdutoDTO;
 import com.brunoglauco.cursomc.services.ProdutoService;
 
 @RestController
@@ -22,5 +26,18 @@ public class ProdutoResource {
 	public ResponseEntity<Produto> find(@PathVariable Integer id) {
 		Produto obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value="nome", defaultValue="0") Integer nome,
+			@RequestParam(value="categorias", defaultValue="0") Integer categorias,
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Produto> list = service.search(page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 }
