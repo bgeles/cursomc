e37c412547980a838fb146bb747bbaf787e98d98
Bruno Glauco Eles <bglauco@corpus.com.br>
2019-05-23 17:31:57 -0300

    adiocionando Produtos

---
+++ src/main/java/com/brunoglauco/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,5 @@
+package com.brunoglauco.cursomc.dto;
+
+public class ProdutoDTO {
+
+}
--- src/main/java/com/brunoglauco/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/brunoglauco/cursomc/repositories/CategoriaRepository.java
@@ -1,11 +1,18 @@
 package com.brunoglauco.cursomc.repositories;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.brunoglauco.cursomc.domain.Categoria;
+import com.brunoglauco.cursomc.domain.Produto;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
+	
+	
 }
