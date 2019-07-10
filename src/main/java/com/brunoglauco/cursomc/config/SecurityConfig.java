99102ac51d8499dcd8db5ec9ad8c71097014b060
Bruno Glauco Eles <bgeles@gmail.com>
2019-07-09 22:37:50 -0300

    Implementando autorização

--- src/main/java/com/brunoglauco/cursomc/config/SecurityConfig.java
+++ src/main/java/com/brunoglauco/cursomc/config/SecurityConfig.java
@@ -65,9 +65,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-			//.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			.anyRequest().hasAnyAuthority();
+			.anyRequest().authenticated();
+			//.anyRequest().hasAnyAuthority();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
